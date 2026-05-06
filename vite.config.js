b132fe34654d0244a2b73243112a303dc343ec0d
nehemiamarbun <nehemiamarbun00@gmail.com>
2026-05-06 13:40:15 +0700

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+
+  base: command === 'build'
+    ? '/portfolio/'
+    : '/'
+}))
